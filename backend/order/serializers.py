af58ca84eef6b3500a72937b0f09e192d40b45c9
maciejas22 <87901496+maciejas22@users.noreply.github.com>
2023-04-02 16:40:31 +0200

    Delete backend directory

--- backend/order/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import Order, OrderItem
-
-class OrderItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OrderItem
-        fields = ('id', 'product', 'quantity')
