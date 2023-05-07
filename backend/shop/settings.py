4c4b20c7e93cc2a386aeecafb94e66aa8b11abf2
maciejas22 <maciejas22@DESKTOP-HD39M9T>
2023-05-07 10:27:28 +0200

    some changes

--- backend/shop/settings.py
+++ backend/shop/settings.py
@@ -10,11 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import json
+import os
 from pathlib import Path
 
-with open("config.json") as config_file:
-    config = json.load(config_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +23,7 @@ from datetime import timedelta
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config["SECRET_KEY"]
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
